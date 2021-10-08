fc02051434ea925abfd133d5544f3262c0d31909
NairoMudumane <nairomudumane@outlook.com>
2021-10-08 11:01:58 +0200

    switching to react-native

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+import {SingIn} from './src/screens/singin/';
+
+export default function App() {
+  return (
+    <>
+      <SingIn />
+    </>
+  );
+}
